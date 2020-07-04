3dbe32225f9622275617dda04c519e3c934f9b0e
Tom Kwong <tk3369@gmail.com>
2020-07-04 12:41:35 -0700

    ✅ migrated tests to the new design

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using LoggingFacilities
+using Test
+
 using Logging
 using JSON
-using Test
+using Dates
 
 @testset "LoggingFacilities.jl" begin
 
@@ -17,9 +19,9 @@ using Test
     # Validate timestamp is printed at the beginning of line
     let
         io = IOBuffer()
-        ts_trans = TimestampTransform(format = "yyyy-mm-dd HH:MM:SS",
-                                      location = InjectByPrependingToMessage())
-        with_logger(logger(SimplestLogger(io), ts_trans)) do
+        logger = build(SimpleLogger(io), inject(BeginningMessageLocation(),
+                                              () -> Dates.format(now(), "yyyy-mm-dd HH:MM:SS")))
+        with_logger(logger) do
             @info "hey there"
         end
         logs = String(take!(io)) |> chomp
@@ -29,8 +31,8 @@ using Test
     # Validate there's only a single line
     let
         io = IOBuffer()
-        oneline_fmt = OneLineTransform()
-        with_logger(logger(SimplestLogger(io), oneline_fmt)) do
+        logger = build(SimplestLogger(io), migrate(KwargsProperty(), MessageProperty()))
+        with_logger(logger) do
             x = 1
             y = "abc"
             @info "hey there" x y
@@ -42,8 +44,11 @@ using Test
     # Verify valid JSON
     let
         io = IOBuffer()
-        js_trans = JSONTransform(indent = 2)
-        json_logger = logger(SimplestLogger(io), js_trans)
+        json_logger = build(SimplestLogger(io),
+                            migrate(LevelProperty(), KwargsProperty()),
+                            migrate(MessageProperty(), KwargsProperty()),
+                            migrate(KwargsProperty(), MessageProperty();
+                                    transform = x -> chomp(json(x, 2)), prepend = ""))
         with_logger(json_logger) do
             x = 1
             y = "abc"
@@ -54,31 +59,5 @@ using Test
         @test JSON.Parser.parse(logs) isa Any
     end
 
-    # LevelAsVarTransform
-    let
-        io = IOBuffer()
-        lv_trans = LevelAsVarTransform()
-        json_logger = logger(SimplestLogger(io), lv_trans)
-        with_logger(json_logger) do
-            @info "hey"
-        end
-        logs = split(String(take!(io)), "\n")
-        @test logs[2] == "level = Info"
-    end
 
-    # Chain transformers
-    let
-        io = IOBuffer()
-        js_trans = JSONTransform(indent = 2)
-        ts_trans = TimestampTransform(format = "yyyy-mm-dd HH:MM:SS", location = InjectByAddingToKwargs())
-        my_logger = logger(SimplestLogger(io), ts_trans, js_trans)
-        with_logger(my_logger) do
-            x = 1
-            y = "abc"
-            z = 36.55
-            @info "hey" x y z
-        end
-        logs = String(take!(io))
-        @test JSON.Parser.parse(logs) isa Any
-    end
 end
